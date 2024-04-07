950542c48dfdf54753dce9151b1cafce7c80c63a
Тимофей <tryatim8@gmail.com>
2024-04-07 15:45:21 +0400

    start working

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from pydantic import SecretStr, StrictStr
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+DATETIME_FORMAT = '%d.%m.%Y - %H:%M:%S'
+DEFAULT_COMMANDS = (
+    ('help', 'Информация по командам'),
+    ('low', 'Предложить товары/услуги по минимальным характеристикам поиска'),
+    ('high', 'Предложить товары/услуги по максимальным характеристикам поиска'),
+    ('custom', 'Предложить товары/услуги по настраиваемым характеристикам поиска'),
+    ('history', 'История поиска')
+)
+
+
+class ConfigSettings(BaseSettings):
+    bot_token: SecretStr = os.getenv('BOT_TOKEN', None)
+    api_key: SecretStr = os.getenv('SITE_API', None)
+    host_api: StrictStr = os.getenv('HOST_API', None)
