480b8f5a73b0fea0a05aa7e356e1164dc2305a69
Тимофей <tryatim8@gmail.com>
2024-04-10 21:12:32 +0400

    changed database structure

--- database/common/models.py
+++ database/common/models.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from config import DATETIME_FORMAT
+
 from peewee import (SqliteDatabase, Model, IntegerField, AutoField,
                     ForeignKeyField, DateTimeField, TextField)
 
@@ -12,15 +13,26 @@ class BaseModel(Model):
 
 class User(BaseModel):
     user_id = IntegerField(primary_key=True)  # Первичный ключ модели, будет совпадать с Telegram ID, уникален.
-    username = TextField()  # Никнейм в telegram
+    user_name = TextField(null=True)  # Никнейм в telegram
     first_name = TextField()  # Имя в telegram
     last_name = TextField(null=True)  # Фамилия в telegram. Может быть не указана, поэтому ставим null=True.
 
 
 class History(BaseModel):
-    command_ord = AutoField()  # Автоматический ID. Это порядковый номер команды.
-    search_time = DateTimeField(default=datetime.now())  # Время запроса
-    user = ForeignKeyField(User, backref='search_history')  # Привязка истории к пользователю
+    command_order = AutoField()  # Автоматический ID. Это порядковый номер команды.
+    search_time = DateTimeField()  # Время запроса
+    user = ForeignKeyField(User, backref='commands_history')  # Привязка истории к пользователю
     command_name = TextField()  # Имя команды
     query_name = TextField(null=True)  # Текст запроса (Название товара или его код)
+    result_size = IntegerField(null=True)  # Количество предложенных товаров
     price_range = TextField(null=True)  # Диапазон цен (команда custom)
+
+    def __str__(self):
+        return '{num} - {dtime}, /{comm}, a-{name}, b-{limit}, c-{range}'.format(
+            num=self.command_order,
+            dtime=self.search_time.strftime(DATETIME_FORMAT),
+            comm=self.command_name,
+            name=self.query_name,
+            limit=self.result_size,
+            range=self.price_range
+        )
