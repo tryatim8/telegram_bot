eb9906b6ec31c1ea55c56bbe916d4f6d2794cf74
Тимофей <tryatim8@gmail.com>
2024-04-11 16:14:19 +0400

    changed echo handler

--- tg_API/handlers/echo.py
+++ tg_API/handlers/echo.py
@@ -1,9 +1,7 @@
 from tg_API.core import bot
-from database.common.models import User, History
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_and_info(message):
-    bot.send_message(message.from_user.id, message.text)
     bot.reply_to(message, 'Для начала работы отправьте /start\n'
                           'Для получения списка команд отправьте /help')
