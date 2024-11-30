d592d7839b71842f171d6fd4cadcb7f7116dfabd
Timofey Prokofyev <tryatim8@gmail.com>
2024-11-30 15:08:24 +0400

    tg and api config

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from pydantic import SecretStr, StrictStr
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+DATETIME_FORMAT = '%d.%m.%Y - %H:%M:%S'
+DEFAULT_COMMANDS = (
+    ('start', 'Начало работы'),
+    ('help', 'Информация по командам'),
+    ('low', 'Предложить товары/услуги по минимальным характеристикам поиска'),
+    ('high', 'Предложить товары/услуги по максимальным характеристикам поиска'),
+    ('custom', 'Предложить товары/услуги по настраиваемым характеристикам поиска'),
+    ('history', 'История поиска')
+)
+RESULT_LIMIT = 15
+
+
+class ConfigSettings(BaseSettings):
+    bot_token: SecretStr = os.getenv('BOT_TOKEN', None)
+    api_key: SecretStr = os.getenv('SITE_API', None)
+    host_api: StrictStr = os.getenv('HOST_API', None)
